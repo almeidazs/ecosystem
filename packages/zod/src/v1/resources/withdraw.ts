7c703170431d49c3b980adecb3682c2c60b6f6d3
almeida <hello@eualmeida.com.br>
2026-01-21 11:08:34 -0300

    feat(@abacatepay/zod): add v1 withdraw schemas

---
+++ packages/zod/src/v1/resources/withdraw.ts
@@ -0,0 +1,57 @@
+import { z } from 'zod';
+import { StringEnum } from '../../utils';
+
+/**
+ * https://docs.abacatepay.com/pages/withdraw/reference#atributos
+ */
+export const WithdrawStatus = StringEnum(
+	['PENDING', 'EXPIRED', 'CANCELLED', 'COMPLETE', 'REFUNDED'],
+	'Transaction status.',
+);
+
+export type WithdrawStatus = z.infer<typeof WithdrawStatus>;
+
+/**
+ * https://docs.abacatepay.com/pages/withdraws/reference
+ */
+export const APIWithdraw = z.object({
+	id: z.string({
+		description: 'Unique transaction identifier.',
+	}),
+	status: WithdrawStatus,
+	devMode: z.boolean({
+		description:
+			'Indicates whether the transaction was created in a testing environment.',
+	}),
+	receiptUrl: z
+		.string({
+			description: 'Transaction proof URL.',
+		})
+		.url(),
+	amount: z
+		.number({
+			description: 'Withdraw value in cents.',
+		})
+		.int(),
+	platformFee: z
+		.number({
+			description: 'Platform fee in cents.',
+		})
+		.int(),
+	externalId: z
+		.string({
+			description: 'External transaction identifier.',
+		})
+		.optional(),
+	createdAt: z.coerce.date({
+		description: 'Transaction creation date.',
+	}),
+	updatedAt: z.coerce.date({
+		description: 'Transaction update date.',
+	}),
+	kind: z.literal('WITHDRAW', {
+		description: "Transaction type. It will always be 'WITHDRAW'",
+	}),
+});
+
+export type APIWithdraw = z.infer<typeof APIWithdraw>;
