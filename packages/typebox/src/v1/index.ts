99cd37510d0f3cdb5c454c2d37f2f91dc9efab78
almeida <hello@eualmeida.com.br>
2026-01-21 09:28:55 -0300

    feat(@abacatepay/typebox): add v1 index.ts

---
+++ packages/typebox/src/v1/index.ts
@@ -0,0 +1,6 @@
+export * from './resources/charge';
+export * from './resources/coupon';
+export * from './resources/customer';
+export * from './resources/pix';
+export * from './resources/store';
+export * from './resources/withdraw';
