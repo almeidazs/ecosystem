be442dc27026aa5b7892a8a1a90a8bf533d013cc
almeida <hello@eualmeida.com.br>
2026-01-29 09:38:31 -0300

    feat(@abacatepay/types): export ABACATEPAY_DOCS_URL

--- packages/types/types/index.ts
+++ packages/types/types/index.ts
@@ -6,3 +6,8 @@ export { API_VERSION, version } from './version';
  * @see {@link https://docs.abacatepay.com/}
  */
 export const API_BASE_URL = 'https://api.abacatepay.com/';
+
+/**
+ * Official URL for the AbacatePay API Documentation
+ */
+export const ABACATEPAY_DOCS_URL = 'https://docs.abacatepay.com/';
